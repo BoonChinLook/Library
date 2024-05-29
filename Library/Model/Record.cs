6a2abefe6bac68c5268ca425ae23b0f6793fc70b
Boon Chin Look <132277248+BoonChinLook@users.noreply.github.com>
2024-05-29 21:38:35 +0100

    Update Record.cs

--- Library/Model/Record.cs
+++ Library/Model/Record.cs
@@ -4,3 +4,33 @@ using System.IO;
 using System.Runtime.Serilization.Formatters.Binary;
 using System.Windows.Forms;
 
+namespace BookListForm
+{
+    [Serializable]
+
+    class Record
+    {
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Genre { get; set; }
+
+        public string BookDescription { get; set; }
+
+        public int DatePublished { get; set; }
+
+        public string Publisher { get; set; }
+
+        public Record(string ti, string au, string ge, string de, string da, string pu)
+        {
+            Title = ti;
+            Author = au;
+            Genre = ge;
+            BookDescription = de;
+            Publisher = da;
+            BookDescription = pu;
+        }
+    }
+}
