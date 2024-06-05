788b228dfa8e9aef06e6ef3f84bc1bb6ac25e0e2
aliona <alionajanusko@gmail.com>
2024-06-05 12:30:10 +0100

    Register form update

--- Library/Forms/RegisterForm.Designer.cs
+++ Library/Forms/RegisterForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegisterForm));
             this.lblUsername = new System.Windows.Forms.Label();
             this.txtUserName = new System.Windows.Forms.TextBox();
             this.lblPassword = new System.Windows.Forms.Label();
@@ -35,76 +36,128 @@
             this.btnRegisterAndLogin = new System.Windows.Forms.Button();
             this.txtRepeatPassword = new System.Windows.Forms.TextBox();
             this.lblRepeatPassword = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Register = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // lblUsername
             // 
             this.lblUsername.AutoSize = true;
-            this.lblUsername.Location = new System.Drawing.Point(288, 111);
+            this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.lblUsername.ForeColor = System.Drawing.Color.Teal;
+            this.lblUsername.Location = new System.Drawing.Point(355, 109);
+            this.lblUsername.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(70, 16);
+            this.lblUsername.Size = new System.Drawing.Size(81, 17);
             this.lblUsername.TabIndex = 4;
             this.lblUsername.Text = "Username";
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(409, 111);
+            this.txtUserName.Location = new System.Drawing.Point(356, 136);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtUserName.MaxLength = 16;
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(98, 22);
+            this.txtUserName.Size = new System.Drawing.Size(203, 20);
             this.txtUserName.TabIndex = 0;
             // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(288, 163);
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.lblPassword.ForeColor = System.Drawing.Color.Teal;
+            this.lblPassword.Location = new System.Drawing.Point(355, 167);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(67, 16);
+            this.lblPassword.Size = new System.Drawing.Size(77, 17);
             this.lblPassword.TabIndex = 5;
             this.lblPassword.Text = "Password";
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(409, 163);
+            this.txtPassword.Location = new System.Drawing.Point(356, 192);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtPassword.MaxLength = 16;
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(98, 22);
+            this.txtPassword.Size = new System.Drawing.Size(203, 20);
             this.txtPassword.TabIndex = 1;
             // 
             // btnRegisterAndLogin
             // 
-            this.btnRegisterAndLogin.Location = new System.Drawing.Point(291, 282);
+            this.btnRegisterAndLogin.BackColor = System.Drawing.Color.Teal;
+            this.btnRegisterAndLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnRegisterAndLogin.ForeColor = System.Drawing.Color.White;
+            this.btnRegisterAndLogin.Location = new System.Drawing.Point(356, 312);
+            this.btnRegisterAndLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnRegisterAndLogin.Name = "btnRegisterAndLogin";
-            this.btnRegisterAndLogin.Size = new System.Drawing.Size(216, 23);
+            this.btnRegisterAndLogin.Size = new System.Drawing.Size(178, 28);
             this.btnRegisterAndLogin.TabIndex = 3;
             this.btnRegisterAndLogin.Text = "Register and back Log In";
-            this.btnRegisterAndLogin.UseVisualStyleBackColor = true;
+            this.btnRegisterAndLogin.UseVisualStyleBackColor = false;
             this.btnRegisterAndLogin.Click += new System.EventHandler(this.btnRegisterAndLogin_Click);
             // 
             // txtRepeatPassword
             // 
-            this.txtRepeatPassword.Location = new System.Drawing.Point(409, 218);
+            this.txtRepeatPassword.Location = new System.Drawing.Point(358, 247);
+            this.txtRepeatPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtRepeatPassword.MaxLength = 16;
             this.txtRepeatPassword.Name = "txtRepeatPassword";
             this.txtRepeatPassword.PasswordChar = '*';
-            this.txtRepeatPassword.Size = new System.Drawing.Size(98, 22);
+            this.txtRepeatPassword.Size = new System.Drawing.Size(203, 20);
             this.txtRepeatPassword.TabIndex = 2;
             // 
             // lblRepeatPassword
             // 
             this.lblRepeatPassword.AutoSize = true;
-            this.lblRepeatPassword.Location = new System.Drawing.Point(288, 218);
+            this.lblRepeatPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.lblRepeatPassword.ForeColor = System.Drawing.Color.Teal;
+            this.lblRepeatPassword.Location = new System.Drawing.Point(353, 223);
+            this.lblRepeatPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblRepeatPassword.Name = "lblRepeatPassword";
-            this.lblRepeatPassword.Size = new System.Drawing.Size(115, 16);
+            this.lblRepeatPassword.Size = new System.Drawing.Size(134, 17);
             this.lblRepeatPassword.TabIndex = 6;
             this.lblRepeatPassword.Text = "Repeat Password";
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(271, 366);
+            this.panel1.TabIndex = 7;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(268, 366);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Register
+            // 
+            this.Register.AutoSize = true;
+            this.Register.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.Register.Location = new System.Drawing.Point(379, 38);
+            this.Register.Name = "Register";
+            this.Register.Size = new System.Drawing.Size(142, 29);
+            this.Register.TabIndex = 8;
+            this.Register.Text = "Registration";
+            // 
             // RegisterForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.Register);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.txtRepeatPassword);
             this.Controls.Add(this.lblRepeatPassword);
             this.Controls.Add(this.btnRegisterAndLogin);
@@ -112,8 +165,11 @@
             this.Controls.Add(this.lblPassword);
             this.Controls.Add(this.txtUserName);
             this.Controls.Add(this.lblUsername);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "RegisterForm";
             this.Text = "RegisterForm";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -128,5 +184,8 @@
         private System.Windows.Forms.Button btnRegisterAndLogin;
         private System.Windows.Forms.TextBox txtRepeatPassword;
         private System.Windows.Forms.Label lblRepeatPassword;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label Register;
     }
 }
